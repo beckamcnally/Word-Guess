9ba9386e4b61ce1390f7d12d8a0722fd383160ec
Becka McNally <beckamcnally@gmail.com>
2023-01-03 17:27:29 -0800

    display blanks working

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var wordBank = ["car", "shea", "doggy", "bensan"]
 
 
-
-
+var guessBtn = document.getElementById("guessbtn")
+var userInput = document.getElementById("userInput")
 var intro = document.querySelector(".intro-box");
 var startBtn = document.querySelector(".start-btn");
 var gameBox = document.querySelector(".game-box");
@@ -10,13 +10,14 @@ var timeleft = document.querySelector(".time-left");
 var resultScreen = document.querySelector(".result-screen");
 var currentWord = "";
 
-startBtn.addEventListener("click", startGame);
+
 
 function startGame() {
     intro.setAttribute("class", "hidden");
     gameBox.removeAttribute("class", "hidden");
 
     getWord();
+   
 }
 
 function getWord() {
@@ -27,15 +28,30 @@ function getWord() {
 
     currentWord = currentW.split('');
     console.log(currentWord);
+    displayBlanks()
 }
 
 function displayBlanks() {
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
-    }
+var gameBoxul = document.createElement("ul")
+gameBox.appendChild(gameBoxul)
+
+    for (var index = 0; index < currentWord.length; index++) {
+        var letters = currentWord[index];
+        var gameBoxli = document.createElement("li")
+        gameBoxul.appendChild(gameBoxli)
+        // gameBox.textContent = letters
+        console.log(letters)
+    }    
 }
 
+function displayCorrect(){
+  var userGuess = userInput.value;
+  console.log(userGuess)
+    // if ()
+}
+
+guessBtn.addEventListener("click", displayCorrect); 
+startBtn.addEventListener("click", startGame);
 
 // Pull up a word
 // convert the word to an array
